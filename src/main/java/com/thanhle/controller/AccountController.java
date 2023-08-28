3b9fbf382242e4caa9793cee70729d3915eaf229
Thanh Le <thanhlevan0203@gmail.com>
2023-08-27 23:28:53 -0700

    updated Account RestController

--- src/main/java/com/thanhle/controller/AccountController.java
+++ src/main/java/com/thanhle/controller/AccountController.java
@@ -26,6 +26,64 @@ public class AccountController {
     @Autowired
     private CustomerService customerService;
     
+ // Create a new account
+    @PostMapping
+    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+    	try {
+    		Account createdAccount = accountService.createAccount(account);
+    		return ResponseEntity.ok(createdAccount);
+    	} catch (IllegalArgumentException e) {
+    		return ResponseEntity.badRequest().build();
+    	}
+ 
+    }
+
+    // Get all accounts
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Account>> getAllAccounts() {
+        return ResponseEntity.ok(accountService.findAllAccounts());
+    }
+
+    // Get account by id
+    @GetMapping("/{accountId}")
+    public ResponseEntity<Account> getAccountById(@PathVariable Long accountId) {
+        Account account = accountService.findAccountById(accountId);
+        if (account != null) {
+            return ResponseEntity.ok(account);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Update an account
+    @PutMapping("/{id}")
+    public ResponseEntity<Account> updateAccount(@PathVariable Long accountId, @RequestBody Account updatedAccount) {
+    	if (!accountId.equals(updatedAccount.getAccountId())) {
+    		return ResponseEntity.badRequest().build();
+    	}
+
+        return ResponseEntity.ok(accountService.updateAccount(updatedAccount));
+    }
+
+    // Delete an account
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // Get accounts by type
+    @GetMapping("/type/{type}")
+    public ResponseEntity<List<Account>> getAccountsByType(@PathVariable AccountType type) {
+        return ResponseEntity.ok(accountService.findByAccountType(type));
+    }
+
+    // Get accounts by customer id
+    @GetMapping("/customer/{customerId}")
+    public ResponseEntity<List<Account>> getAccountsByCustomerId(@PathVariable Long customerId) {
+        return ResponseEntity.ok(accountService.findAccountsByCustomerId(customerId));
+    }
+    
     @GetMapping
 	public String getAccountForm(Model model) {
 		
@@ -64,52 +122,5 @@ public class AccountController {
 	
 	
 
-	// Create a new account
-    @PostMapping
-    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
-        return ResponseEntity.ok(accountService.createAccount(account));
-    }
-
-    // Get all accounts
-    @GetMapping("/getAll")
-    public ResponseEntity<List<Account>> getAllAccounts() {
-        return ResponseEntity.ok(accountService.findAllAccounts());
-    }
-
-    // Get account by id
-    @GetMapping("/{id}")
-    public ResponseEntity<Account> getAccountById(@PathVariable Long id) {
-        Account account = accountService.findAccountById(id);
-        if (account != null) {
-            return ResponseEntity.ok(account);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    // Update an account
-    @PutMapping("/{id}")
-    public ResponseEntity<Account> updateAccount(@PathVariable Long id, @RequestBody Account updatedAccount) {
-        updatedAccount.setAccountId(id);  // Ensure ID matches
-        return ResponseEntity.ok(accountService.updateAccount(updatedAccount));
-    }
-
-    // Delete an account
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteAccount(@PathVariable Long id) {
-        accountService.deleteAccount(id);
-        return ResponseEntity.noContent().build();
-    }
-
-    // Get accounts by type
-    @GetMapping("/type/{type}")
-    public ResponseEntity<List<Account>> getAccountsByType(@PathVariable AccountType type) {
-        return ResponseEntity.ok(accountService.findByAccountType(type));
-    }
-
-    // Get accounts by customer id
-    @GetMapping("/customer/{customerId}")
-    public ResponseEntity<List<Account>> getAccountsByCustomerId(@PathVariable Long customerId) {
-        return ResponseEntity.ok(accountService.findAccountsByCustomerId(customerId));
-    }
+	
 }
